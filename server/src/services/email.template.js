761993b2004b1ff2713deed9e6d52c378bd40b4c
Hazem Mohamed <hazem1abc@gmail.com>
2023-07-09 01:49:26 +0300

    Update email.template.js

--- server/src/services/email.template.js
+++ server/src/services/email.template.js
@@ -154,7 +154,7 @@ export const emailHtml = (email) => {
                     <table border="0" cellpadding="0" cellspacing="0">
                       <tr>
                         <td align="center" bgcolor="#1a82e2" style="border-radius: 6px;">
-                          <a href="http://localhost:3000/api/v1/user/verify/${email}" target="_blank" style="display: inline-block; padding: 16px 36px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; color: #ffffff; text-decoration: none; border-radius: 6px;">Confirm Email</a>
+                          <a href="https://taski-server.vercel.app/api/v1/user/verify/${email}" target="_blank" style="display: inline-block; padding: 16px 36px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; color: #ffffff; text-decoration: none; border-radius: 6px;">Confirm Email</a>
                         </td>
                       </tr>
                     </table>
